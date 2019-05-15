53cec314dae3a74d06d64084411a25eea5c187a7
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-05-15 22:26:51 +0300

    done tests and passed for single character word encryption, numeric strings and spaces

--- src/main/java/CeaserEncrypt.java
+++ src/main/java/CeaserEncrypt.java
@@ -1,5 +1,41 @@
+import com.sun.tools.javac.util.ArrayUtils;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.lang.Character;
+
 public class CeaserEncrypt {
-    public String textEncrypt(String text, int shift){
+    public String textEncrypt(String text, int shiftKey){
+        if(text.matches(".*\\d.*")){
+            return text;
+        }else if(text.matches(" ")){
+            return text;
+        }else
+            if(text.matches("[a-zA-Z]")){
+            //char[] charText=text.toCharArray();
+            //Character[] charObjectText= ArrayUtils.toObject(charText);
+//            Character[] charObjectText= text.chars().mapToObj(c -> (char)c).toArray(Character[]::new);
+//            ArrayList<Character> charResult=new ArrayList<Character>();
+//
+//
+//            for(int index=0;index<charObjectText.length; index++){
+//                if(charObjectText[index].equals("[a-zA-Z]")){
+//                    Character cipheredText=(char)(charObjectText[index]+shiftKey);
+//                    charResult.add(cipheredText);
+//                }
+//            }
+//            return new String(charResult);
+
+
+            String strIncremented=new String();
+            for(int i=0;i<text.length();i++){
+                strIncremented+=(char)(text.charAt(i)+shiftKey);
+            }
+            return (strIncremented);
+
+        } else
         return null;
     }
 }
--- src/test/java/CeaserEncryptTest.java
+++ src/test/java/CeaserEncryptTest.java
@@ -1,5 +1,26 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CeaserEncryptTest {
+    @Test
+    public void textEncrypt_stringNumberRemainsUnchanged_String() {
+        CeaserEncrypt testString = new CeaserEncrypt();
+        String stringNumber="1";
+        assertEquals(stringNumber,testString.textEncrypt("1",1));
+    }
 
+    @Test
+    public void textEncrypt_spaceRemainsSpace_String() {
+        CeaserEncrypt testString = new CeaserEncrypt();
+        String stringSpace=" ";
+        assertEquals(stringSpace,testString.textEncrypt(" ",1));
+    }
+
+    @Test
+    public void textEncrypt_singleLetterShiftsByKey() {
+        CeaserEncrypt testString = new CeaserEncrypt();
+        String stringLetter="D";
+        assertEquals(stringLetter,testString.textEncrypt("A",3));
+    }
 }
