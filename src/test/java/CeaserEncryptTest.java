ad79c07fd427a001350bb33595f8a7e418c4520b
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-05-16 11:52:14 +0300

    adapted encryption tests to encapsulated class member variables

--- src/test/java/CeaserEncryptTest.java
+++ src/test/java/CeaserEncryptTest.java
@@ -1,61 +1,140 @@
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//
+//public class CeaserEncryptTest {
+//    @Test
+//    public void textEncrypt_stringNumberRemainsUnchanged_String() {
+//        CeaserEncrypt testString = new CeaserEncrypt();
+//        String stringNumber="1";
+//        assertEquals(stringNumber,testString.textEncrypt("1",1));
+//    }
+//
+//    @Test
+//    public void textEncrypt_spaceRemainsSpace_String() {
+//        CeaserEncrypt testString = new CeaserEncrypt();
+//        String stringSpace=" ";
+//        assertEquals(stringSpace,testString.textEncrypt(" ",1));
+//    }
+//
+//    @Test
+//    public void textEncrypt_singleLetterEncryption_String() {
+//        CeaserEncrypt testString = new CeaserEncrypt();
+//        String stringLetter="D";
+//        assertEquals(stringLetter,testString.textEncrypt("A",3));
+//    }
+//
+//    @Test
+//    public void textEncrypt_singleWordEncryption_String() {
+//        CeaserEncrypt testString = new CeaserEncrypt();
+//        String stringResult="DBU";
+//        assertEquals(stringResult,testString.textEncrypt("CAT",1));
+//    }
+//
+//    @Test
+//    public void textEncrypt_fullSentenceEncrypt_String(){
+//        CeaserEncrypt testString = new CeaserEncrypt();
+//        String stringResult="J BN B HJSM";
+//        assertEquals(stringResult,testString.textEncrypt("I AM A GIRL",1));
+//    }
+//
+//    @Test
+//    public void textEncrypt_dontEncryptNonAlphabets_String() {
+//        CeaserEncrypt testString = new CeaserEncrypt();
+//        String stringResult="?";
+//        assertEquals(stringResult,testString.textEncrypt("?",1));
+//    }
+//
+//    @Test
+//    public void textEncrypt_lowerUpperCaseEncryption_String() {
+//        CeaserEncrypt testString=new CeaserEncrypt();
+//        String stringResult="B dbU";
+//        assertEquals(stringResult,testString.textEncrypt("A caT",1));
+//    }
+//
+//    @Test
+//    public void textEncrypt_alphabetEncryptLoopsWhenReachesZ_String() {
+//        CeaserEncrypt testString=new CeaserEncrypt();
+//        String stringResult="WKH TXLFN EURZQ IRA MXPSV RYHU WKH ODCB GRJ";
+//        assertEquals(stringResult,testString.textEncrypt("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG",3));
+//    }
+//}
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CeaserEncryptTest {
     @Test
+    public void newCeaserEncrypt_instantiatesCorrectly() {
+        CeaserEncrypt testString=new CeaserEncrypt("1",1);
+        assertEquals(true, testString instanceof CeaserEncrypt);
+    }
+
+    @Test
+    public void newCeaserEncrypt_getsPlainTextCorrectly_String() {
+        CeaserEncrypt testString=new CeaserEncrypt("Hello girl!",1);
+        assertEquals("Hello girl!",testString.getPlainText());
+    }
+
+    @Test
+    public void newCeaserEncrypt_getsShiftKeyCorrectly_String() {
+        CeaserEncrypt testString=new CeaserEncrypt("Hello girl!",1);
+        assertEquals(1,testString.getShiftKey());
+    }
+
+    @Test
     public void textEncrypt_stringNumberRemainsUnchanged_String() {
-        CeaserEncrypt testString = new CeaserEncrypt();
+        CeaserEncrypt testString = new CeaserEncrypt("1",1);
         String stringNumber="1";
-        assertEquals(stringNumber,testString.textEncrypt("1",1));
+        assertEquals(stringNumber,testString.textEncrypt(testString.getPlainText(),testString.getShiftKey()));
     }
 
     @Test
     public void textEncrypt_spaceRemainsSpace_String() {
-        CeaserEncrypt testString = new CeaserEncrypt();
+        CeaserEncrypt testString = new CeaserEncrypt(" ",1);
         String stringSpace=" ";
-        assertEquals(stringSpace,testString.textEncrypt(" ",1));
+        assertEquals(stringSpace,testString.textEncrypt(testString.getPlainText(),testString.getShiftKey()));
     }
 
     @Test
     public void textEncrypt_singleLetterEncryption_String() {
-        CeaserEncrypt testString = new CeaserEncrypt();
+        CeaserEncrypt testString = new CeaserEncrypt("A",3);
         String stringLetter="D";
-        assertEquals(stringLetter,testString.textEncrypt("A",3));
+        assertEquals(stringLetter,testString.textEncrypt(testString.getPlainText(),testString.getShiftKey()));
     }
 
     @Test
     public void textEncrypt_singleWordEncryption_String() {
-        CeaserEncrypt testString = new CeaserEncrypt();
+        CeaserEncrypt testString = new CeaserEncrypt("CAT",1);
         String stringResult="DBU";
-        assertEquals(stringResult,testString.textEncrypt("CAT",1));
+        assertEquals(stringResult,testString.textEncrypt(testString.getPlainText(),testString.getShiftKey()));
     }
 
     @Test
     public void textEncrypt_fullSentenceEncrypt_String(){
-        CeaserEncrypt testString = new CeaserEncrypt();
+        CeaserEncrypt testString = new CeaserEncrypt("I AM A GIRL",1);
         String stringResult="J BN B HJSM";
-        assertEquals(stringResult,testString.textEncrypt("I AM A GIRL",1));
+        assertEquals(stringResult,testString.textEncrypt(testString.getPlainText(),testString.getShiftKey()));
     }
 
     @Test
     public void textEncrypt_dontEncryptNonAlphabets_String() {
-        CeaserEncrypt testString = new CeaserEncrypt();
+        CeaserEncrypt testString = new CeaserEncrypt("?",1);
         String stringResult="?";
-        assertEquals(stringResult,testString.textEncrypt("?",1));
+        assertEquals(stringResult,testString.textEncrypt(testString.getPlainText(),testString.getShiftKey()));
     }
 
     @Test
     public void textEncrypt_lowerUpperCaseEncryption_String() {
-        CeaserEncrypt testString=new CeaserEncrypt();
+        CeaserEncrypt testString=new CeaserEncrypt("A caT",1);
         String stringResult="B dbU";
-        assertEquals(stringResult,testString.textEncrypt("A caT",1));
+        assertEquals(stringResult,testString.textEncrypt(testString.getPlainText(),testString.getShiftKey()));
     }
 
     @Test
     public void textEncrypt_alphabetEncryptLoopsWhenReachesZ_String() {
-        CeaserEncrypt testString=new CeaserEncrypt();
+        CeaserEncrypt testString=new CeaserEncrypt("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG",3);
         String stringResult="WKH TXLFN EURZQ IRA MXPSV RYHU WKH ODCB GRJ";
-        assertEquals(stringResult,testString.textEncrypt("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG",3));
+        assertEquals(stringResult,testString.textEncrypt(testString.getPlainText(),testString.getShiftKey()));
     }
 }
